a949a091eaf282d8f90cd8eccfc2192bf91bf963
hawkey <hawkey1999@126.com>
2018-04-06 17:57:09 +0800

    add

---
+++ MainWindow.java
@@ -0,0 +1,32 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MainWindow extends JFrame {
+    public static final int DEBUG=1;
+    public static void main(String[] args) {
+        MainWindow m=new MainWindow();
+    }
+    private MainPanel P=new MainPanel();
+    private static final int WIN_WIDTH=915;
+    private static final int WIN_HEIGHT=615;
+
+    public static int getWinWidth(){
+        return WIN_WIDTH;
+    }
+
+    public static int getWinHeight(){
+        return WIN_HEIGHT;
+    }
+
+    private MainWindow(){
+        setSize(WIN_WIDTH,WIN_HEIGHT);
+        setResizable(false);
+        setTitle("Charraria");
+        this.getContentPane().setBackground(Color.WHITE);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        add(P, BorderLayout.CENTER);
+        setVisible(true);
+    }
+
+
+}
